b74367829945a102bc97a353c7354fa39eb60bc4
kei <tenguuenglish@gmail.com>
2020-01-01 15:48:43 +0900

    :construction: Show restaurants but for testing

--- src/components/Restaurants/IndexPage.tsx
+++ src/components/Restaurants/IndexPage.tsx
@@ -1,11 +1,31 @@
 /** @jsx jsx */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { jsx } from '@emotion/core';
 import { AppHeader } from '../AppHeader';
 
-export const RestaurantsIndexPage = () => (
-  <div>
-    <AppHeader />
-    <h1>Hello, world!</h1>
-  </div>
-);
+export const RestaurantsIndexPage = () => {
+  const [restaurants, setRestaurants] = useState<any[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const res = await fetch(
+        'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=e0282a617864a747ca5ed4c9566bfa46&lunch=1',
+      );
+      const result = await res.json();
+      setRestaurants(result.rest);
+    };
+    load();
+  }, []);
+
+  return (
+    <div>
+      <AppHeader />
+      {restaurants.map(r => (
+        <div key={r.id}>
+          <span>{r.name}</span>
+          <img src={r.image_url.shop_image1} alt="restaurant" />
+        </div>
+      ))}
+    </div>
+  );
+};
